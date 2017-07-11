1cd15af300423a8fb00037fa2e7691d521a72006
Ryan <lauer.ryan@ymail.com>
2017-07-10 21:21:59 -0400

    adding

--- src/app/search/common/app.routes.ts
+++ src/app/search/common/app.routes.ts
@@ -1,10 +1,11 @@
 import {Routes} from "@angular/router";
+import {SearchComponent} from "../search.component";
 /**
  * Created by ryanlauer on 6/8/17.
  */
 export const ROUTES: Routes = [
 {
   path: 'contact',
-    component: ContactComponent
+    component: SearchComponent
 }
 ];
--- src/app/search/common/hwsbot-service.ts
+++ src/app/search/common/hwsbot-service.ts
@@ -1,17 +1,18 @@
 import {HttpClient} from "./http-client";
 import {Injectable} from "@angular/core";
-import { SearchComponent } from '../search.component'
+import {SearchComponent} from '../search.component'
 /**
  * Created by ryanlauer on 6/8/17.
  */
 @Injectable
-export class HwsBotService {
-   constructor (private httpClient: HttpClient) {}
+ export class HwsBotService {
+  constructor(private httpClient: HttpClient) {
+  }
 
-   getItemQuote(item: String) {
-     return this.httpClient
-       .post(`${apis.hwsApi}/search-item`, {item})
-       .map(this.httpClient.extractData)
-       .catch(this.httpClient.handleError);
-   }
+  getItemQuote(item: String) {
+    return this.httpClient
+      .post(`${apis.hwsApi}/search-item`, {item})
+      .map(this.httpClient.extractData)
+      .catch(this.httpClient.handleError);
+  }
 }
